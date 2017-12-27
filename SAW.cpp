974be1f2cad9872e7499802073ef81b005674c44
dinanuroktavia <32434972+dinanuroktavia@users.noreply.github.com>
2017-12-27 23:48:02 +0800

    CLASS ATM

--- SAW.cpp
+++ SAW.cpp
@@ -5,4 +5,15 @@
 using namespace std;
 
 
-
+class atm{
+  public:
+   atm();
+   int login();
+   int display();
+         
+  private:
+   int coba = 0,pilih,no;
+   char pass[100],yes;
+   float s,saldo,Transfer,ambil;
+ };
+ 
